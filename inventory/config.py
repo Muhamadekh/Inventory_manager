d9e8d9f346feb4a8e3dce5d7c7bf6758a16c0f65
unknown <muhamadekhhaji99@gmail.com>
2023-08-08 19:48:24 +0300

    Bugfix

--- inventory/config.py
+++ inventory/config.py
@@ -1,11 +1,19 @@
 import os
 import json
 
+# Production
 with open("/etc/config.json") as config_file:
-    config = json.load(config_file)
+        config = json.load(config_file)
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = config.get("SECRET_KEY")
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = config.get("SQLALCHEMY_DATABASE_URI")
+
+
+# Development
+# class Config:
+#     SECRET_KEY = os.environ.get("SECRET_KEY")
+#     # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+#     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
