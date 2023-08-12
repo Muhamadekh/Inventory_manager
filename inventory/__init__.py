0f38c8702235329e6e911ed75e1a28b9836a86f6
unknown <muhamadekhhaji99@gmail.com>
2023-08-12 19:31:30 +0300

    Bugfix

--- inventory/__init__.py
+++ inventory/__init__.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from inventory.config import Config
+from flask_cors import CORS
 
 
 app = Flask(__name__)
@@ -11,6 +12,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, render_as_batch=True)
 bcrypt = Bcrypt(app)
+cors = CORS()
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message = ""
